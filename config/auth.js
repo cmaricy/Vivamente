5efd6678866cdf6f9b36639e543fcaca64c81fb6
Lucas Costa <lbc.lucascosta@gmail.com>
2016-10-16 23:48:09 -0200

    Init

--- config/auth.js
+++ config/auth.js
@@ -3,6 +3,6 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' : cfg.CLIENT_ID,
 		'clientSecret' : cfg.CLIENT_SECRET,
-		'callbackURL' : 'https://aqueous-basin-41147.herokuapp.com/auth/facebook/callback'
+		'callbackURL' : 'https://frozen-thicket-68161.herokuapp.com/auth/facebook/callback'
 	}
 };
