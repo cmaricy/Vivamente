5bb8e3106d803441626eb278ad6aa0624f07dcbe
Lucas Costa <lbc.lucascosta@gmail.com>
2016-10-21 00:42:12 -0200

    Commit Heroku

--- config/auth.js
+++ config/auth.js
@@ -16,6 +16,6 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID' : cfg.CLIENT_ID,
 		'clientSecret' : cfg.CLIENT_SECRET,
-		'callbackURL' : 'https://frozen-thicket-68161.herokuapp.com/auth/facebook/callback'
+		'callbackURL' : 'https://apps.facebook.com/vivamente'
 	}
 };
